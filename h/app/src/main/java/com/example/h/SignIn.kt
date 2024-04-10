576386374080d82c9758ca2ac75f69cbbdbb5a77
Benjamin030810 <benjaminncw-wm21@student.tarc.edu.my>
2024-04-10 15:54:34 +0800

    added navigation

---
+++ h/app/src/main/java/com/example/h/SignIn.kt
@@ -0,0 +1,23 @@
+package com.example.h
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class SignIn : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_sign_in, container, false)
+
+        val tv : TextView = view.findViewById(R.id.lblName)
+        tv.
+
+        return view
+    }
+}
