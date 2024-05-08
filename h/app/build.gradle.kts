35083247a3d220a3b5428c72ca0ae2fbdd9a8941
CodeOfTerminator <114341536+CodeOfTerminator@users.noreply.github.com>
2024-05-09 01:31:29 +0800

    sync xia nage recylerview then can use already no need spec the 1.3.2 the json file use my one

--- h/app/build.gradle.kts
+++ h/app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
