f165770891ca4ebce0ed17db9ab367530a6545db
Benjamin030810 <benjaminncw-wm21@student.tarc.edu.my>
2024-05-09 16:32:19 +0800

    4 layer

--- h/app/build.gradle.kts
+++ h/app/build.gradle.kts
@@ -37,6 +37,8 @@ android {
 }
 
 dependencies {
+    implementation("com.github.bumptech.glide:glide:5.0.0-rc01")
+    implementation("androidx.preference:preference:1.2.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
@@ -45,7 +47,8 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.firebase.database)
+    implementation("com.google.firebase:firebase-database:20.2.1")
+    implementation("com.google.firebase:firebase-storage:20.3.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
