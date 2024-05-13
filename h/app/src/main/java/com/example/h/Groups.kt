9cf1ae7c947bcb54429686e311cf889a8b9abf49
Benjamin030810 <benjaminncw-wm21@student.tarc.edu.my>
2024-05-13 15:13:31 +0800

    toolbar and navigation drawer

--- h/app/src/main/java/com/example/h/Groups.kt
+++ h/app/src/main/java/com/example/h/Groups.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.h.dataAdapter.GroupAdapter
@@ -17,6 +19,13 @@ class Groups : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_groups, container, false)
+
+        val toolbar : Toolbar = view.findViewById(R.id.toolbarGroups)
+
+        toolbar.setNavigationOnClickListener {
+            (requireActivity() as MainActivity).openDrawer()
+        }
+
 
         val recyclerView : RecyclerView = view.findViewById(R.id.recyclerViewGroupGroups)
         recyclerView.adapter = GroupAdapter()
