ace342dd8b8f040da1c40576099557e437a3e3f4
sauweiiiii~ <114349729+ThongSauWei@users.noreply.github.com>
2024-05-15 23:37:13 +0800

    aa

--- h/app/src/main/java/com/example/h/Home.kt
+++ h/app/src/main/java/com/example/h/Home.kt
@@ -2,19 +2,16 @@ package com.example.h
 
 import FilterPost
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.widget.AppCompatButton
 import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.h.dao.PostDAO
-import com.example.h.data.Post
 import com.example.h.dataAdapter.PostAdapter
 import com.example.h.repository.PostRepository
 import com.example.h.viewModel.PostViewModel
@@ -25,15 +22,7 @@ import kotlinx.coroutines.launch
 class Home : Fragment() {
 
     private lateinit var postAdapter: PostAdapter
-    private lateinit var postRepository: PostRepository
     private lateinit var postViewModel: PostViewModel
-
-    private var selectedCategory: String? = null
-    private var selectedLearningStyle: String? = null
-
-
-    //private lateinit var btnAddHome: AppCompatButton
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -48,29 +37,17 @@ class Home : Fragment() {
         arguments?.let {
             val selectedCategory = arguments?.getString("selectedCategory")
             val selectedLearningStyle = arguments?.getString("selectedLearningStyle")
-            //selectedCategory = it.getString("selectedCategory")
-            //selectedLearningStyle = it.getString("selectedLearningStyle")
-
             val cardViewCriteriaHome = view.findViewById<CardView>(R.id.cardViewCriteriaHome)
             val cardViewCriteria2Home = view.findViewById<CardView>(R.id.cardViewCriteria2Home)
 
             if (selectedCategory != null && selectedLearningStyle != null) {
-                Log.d("selectedCategoryH", selectedCategory)
                 tvCriteriaHome.text = selectedCategory.toString()
                 cardViewCriteriaHome.visibility = View.VISIBLE
 
-//            if (selectedLearningStyle != null) {
-                Log.d("selectedLearningStyleH", selectedLearningStyle)
                 tvCriteria2Home.text = selectedLearningStyle.toString()
                 cardViewCriteria2Home.visibility = View.VISIBLE
-
-                //fetchPostsCategory()
             }
-
         }
-
-
-        //btnAddHome = view.findViewById(R.id.btnAddHome)
 
         val recyclerView: RecyclerView = view.findViewById(R.id.recyclerViewPostHome)
         recyclerView.layoutManager = LinearLayoutManager(activity?.application)
@@ -87,31 +64,21 @@ class Home : Fragment() {
 
         val btnFilterHome = view.findViewById<ImageView>(R.id.btnFilterHome)
         btnFilterHome.setOnClickListener {
-            // Navigate to FilterPost fragment
             val fragment = FilterPost()
             val transaction = activity?.supportFragmentManager?.beginTransaction()
             transaction?.replace(R.id.fragmentContainerView, fragment)
             transaction?.addToBackStack(null) // Add to back stack
             transaction?.commit()
-
         }
 
         // Initialize the PostAdapter
         postAdapter = PostAdapter(emptyList())
         recyclerView.adapter = postAdapter
 
-        // Initialize the PostRepository
-        postRepository = PostRepository(PostDAO())
+        // Initialize the ViewModel
+        postViewModel = ViewModelProvider(this).get(PostViewModel::class.java)
 
-        // Fetch posts from Firebase and set them directly in the adapter
-        //fetchPosts()
-
-//        if (!selectedCategory.isNullOrBlank() && !selectedLearningStyle.isNullOrBlank()) {
-//            fetchPostsByCategoryAndLearningStyle()
-//        } else {
-//            fetchPosts()
-//        }
-
+        // Fetch posts
         if (tvCriteriaHome.text == "ALL") {
             fetchPosts()
         } else {
@@ -124,12 +91,10 @@ class Home : Fragment() {
     private fun fetchPosts() {
         GlobalScope.launch(Dispatchers.Main) {
             try {
-                val posts = postRepository.getAllPost()
-                // Set the fetched posts directly in the adapter
+                val posts = postViewModel.getAllPost()
                 postAdapter.postList = posts
                 postAdapter.notifyDataSetChanged()
             } catch (e: Exception) {
-                // Handle exception
                 e.printStackTrace()
             }
         }
@@ -138,36 +103,12 @@ class Home : Fragment() {
     private fun fetchPosts1(category : String, learningStyle : String) {
         GlobalScope.launch(Dispatchers.Main) {
             try {
-                val posts = postRepository.getPostByCategoryAndLearningStyle(category, learningStyle)
-                // Set the fetched posts directly in the adapter
+                val posts = postViewModel.getPostByCategoryAndLearningStyle(category, learningStyle)
                 postAdapter.postList = posts
                 postAdapter.notifyDataSetChanged()
             } catch (e: Exception) {
-                // Handle exception
                 e.printStackTrace()
             }
         }
     }
-
-//    private fun fetchPostsByCategoryAndLearningStyle() {
-//        GlobalScope.launch(Dispatchers.Main) {
-//            try {
-//                val posts = selectedCategory?.let {
-//                    selectedLearningStyle?.let { it1 ->
-//                        postViewModel.getPostByCategoryAndLearningStyle(
-//                            it, it1
-//                        )
-//                    }
-//                }
-//                // Set the fetched posts directly in the adapter
-//                if (posts != null) {
-//                    postAdapter.postList = posts
-//                }
-//                postAdapter.notifyDataSetChanged()
-//            } catch (e: Exception) {
-//                // Handle exception
-//                e.printStackTrace()
-//            }
-//        }
-//    }
 }
--- h/app/src/main/java/com/example/h/dataAdapter/FilterAdapter.kt
+++ h/app/src/main/java/com/example/h/dataAdapter/FilterAdapter.kt
@@ -1,7 +1,9 @@
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.h.R
 
@@ -26,15 +28,18 @@ class FilterAdapter(private val list: List<String>) : RecyclerView.Adapter<Filte
         val currentItem = list[position]
         holder.btn.text = currentItem
 
+        // Set background color based on selection
+        if (currentItem == selectedItem) {
+            holder.btn.setBackgroundColor(Color.parseColor("#FF9C26"))
+        }
+
+
         // Set click listener for each item
         holder.btn.setOnClickListener {
             // Set the selected item
             selectedItem = currentItem
             notifyDataSetChanged() // Notify adapter of data change
         }
-
-        // Set background color based on selection
-        holder.itemView.isSelected = currentItem == selectedItem
     }
 
     // Method to get the selected value
